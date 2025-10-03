9cf5b52223e5aa29d3bfb5f6eb01038337425082
Tanishk-Alert <tanishkmarothiya@alertenterprise.com>
2025-10-03 12:14:18 +0530

    new code

---
+++ EC2/variable.tf
@@ -0,0 +1,31 @@
+variable "ami" {
+  description = "AMI ID to use for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the instance will be created"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair name"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name tag for the instance"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Additional tags"
+  type        = map(string)
+  default     = {}
+}
