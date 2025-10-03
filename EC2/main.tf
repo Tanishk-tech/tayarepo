5c5e45feb64129730f914682aafa3e69a7c68f7d
Tanishk-Alert <tanishkmarothiya@alertenterprise.com>
2025-10-03 12:49:28 +0530

    new code

--- EC2/main.tf
+++ EC2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "this" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-  key_name      = var.key_name
+  key_name      = "${var.common_tags["env"]}-${var.common_tags["proj"]}-keypair"
 
   tags = merge(
     {
--- ec2-pair/main.tf
+++ ec2-pair/main.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "keypair" {
-  key_name   = "${var.common_tags["env"]}-${var.common_tags["proj"]}-kaypair"
+  key_name   = "${var.common_tags["env"]}-${var.common_tags["proj"]}-keypair"
   public_key = var.public_key
 }
